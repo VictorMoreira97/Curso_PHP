683d2c9d994449916f236658ad37ae934db40bb9
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2023-09-24 09:40:10 -0300

    m

--- cad.php
+++ cad.php
@@ -1,3 +1,24 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+        <?php
+            $nome = $_GET["nome"] ?? "Vazio";
+            $email = $_GET["email"] ?? "Vazio";
+            echo "<p>Bem-vind@, $nome</p><br>";
+            echo "<p>Seu email é: $email</p>"
+        ?>
 
-?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página inicial</a></p>
+    </main>
+</body>
+</html>
