2fb3587e2a5b6dba55ab3d50a06bc090b25acf3d
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2023-09-25 16:41:44 -0300

    m

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html lang='pt-br'>
+<html lang="pt-br">
 <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Me Livrando da Maldição</title>
 </head>
 <body>
     
     <?php
-        echo '<p>echo:<p><br>
+        echo "<p>echo:<p><br>
         <h1>
             Hello World!
-        </h1>';
+        </h1>";
 
-        print '<p>print:<p><br>
+        print "<p>print:<p><br>
         <h1>
             Hello World!
-        </h1>';
+        </h1>";
 
     ?>
 
--- index2.php
+++ index2.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html lang='pt-br'>
+<html lang="pt-br">
 <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testando scripsts PHP</title>
 </head>
 <body>
     <h1>Testando PHP</h1>
     
     <?php 
-        date_default_timezone_set('America/Sao_Paulo');
-        echo 'Hoje é dia '. date('d/H/Y');
-        echo '. São '. date('G:i:s T');
+        date_default_timezone_set("America/Sao_Paulo");
+        echo "Hoje é dia ". date("d/H/Y");
+        echo ". São ". date("G:i:s T");
     ?>
 
 </body>
